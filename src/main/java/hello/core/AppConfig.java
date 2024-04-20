a94b1759bdc3cc9d7154d732dd5e007b82f84150
jk <drakedk@naver.com>
2024-04-20 16:49:40 +0900

    [추가] start spring - AppConfig 리펙토링
    - AppConfig 역할에 따른 구현이 잘 보이도록 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,17 +10,32 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    /**
+     *  각 메서드명을 보는 순간 역할이 드러난다.
+     **/
     public MemberService memberService(){
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    /** 주문 서비스 역할 */
     public OrderService orderService(){
         // 생성자 주입
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    /** 회원 저장소 역할 */
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    /** 할인 정책 역할 */
+    public DiscountPolicy discountPolicy(){
+        // 고정 할인 구현
+        return new FixDiscountPolicy();
     }
 
 }
