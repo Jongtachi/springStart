9a1334b1fc7ce6091c737d731dd1d9a27241b468
jk <drakedk@naver.com>
2024-06-10 23:16:49 +0900

    [추가] start spring - 스코프와 프록시
    - 프록시 애노테이션을 통해 CGLIB라는 라이브러리로 내 클래스를 상속 받은 가짜 프록시 객체를 만들어서 주입 (마치 싱글톤처럼 동작함)
    - 단지 애노테이션 설정 변경만으로 원본 객체를 프록시 객체로 대체할 수 있다. 이것이 바로 다형성과 DI 컨테이너가 가진 큰 강점이다.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,19 +13,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    private final MyLogger myLogger;
 
     // 문자를 바로 봔환하기 위해 @ResponseBody 사용
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+        //MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         
-        // 1초 뒤 service 실행 그 사이 다른 고객들이 들어와도 uuid는 고객마다 다르게 유지 
+        // 1초 뒤 service 실행 그 사이 다른 고객들이 들어와도 uuid는 고객마다 다르게 유지
         Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
